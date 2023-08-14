5e8a04096a8d4bcfb8afe94f1154671490dac20d
grey123son <94587086+grey123son@users.noreply.github.com>
2023-08-14 07:47:59 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
+noseX=0;
+noseY=0;
+leftWristX=0;
+rightWristX=0;
+difference=0;
 function setup(){
     video = createCapture(VIDEO);
     video.size(550,550);
-    video.position(360,150)
+    video.position(360,175);
 
     canvas = createCanvas(550,550);
-    canvas.position(940, 150);
+    canvas.position(940, 200);
 
     poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
 }
 function draw(){
     background("#F5DEB3");
+    document.getElementById("squareSize").innerHTML = "Font size of the square will be:"+difference;
+    text('Greyson',noseX,noseY)
+    textSize(difference);
 }
 function modelLoaded(){
     console.log("PoseNet Initialized! ")
@@ -17,5 +26,18 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.x;
+        console.log("noseX ="+noseX+"noseY ="+noseY)
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        if(rightWristX>leftWristX){
+            difference=floor(rightWristX-leftWristX);
+        }
+        if(leftWristX>rightWristX){
+            difference=floor(leftWristX-rightWristX)
+        }
+        console.log("leftWristX ="+leftWristX+"rightWristX ="+rightWristX+"difference ="+difference);
     }
 }
